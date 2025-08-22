0cbba66b1f981eca9ae349452e80c7b3d682d476
Uzmaa7 <uk9971khan@gmail.com>
2025-08-22 18:50:42 +0530

    feat: add FewShotPosts class for loading post

---
+++ few_shot.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+
+class FewShotPosts:
+    def __init__(self, file_path="data/processed_posts.json"):
+        self.df = None
+        self.unique_tags = None
+        self.load_posts(file_path)
+
+    def load_posts(self, file_path):
+        with open(file_path, encoding="utf-8") as f:
+            posts = json.load(f)
+            self.df = pd.json_normalize(posts)
+            self.df["length"] = self.df["line_count"].apply(self.categorize_length)
+            all_tags = self.df['tags'].apply(lambda x:x).sum()
+            self.unique_tags = set(list(all_tags))
+
+
+
+
+    def get_tags(self):
+        return self.unique_tags
+
+
+
+
+if __name__ == "__main__":
+    fs = FewShotPosts()
+    # posts = fs.get_filtered_posts("Short","English","Job Search")
+    # print(posts)
